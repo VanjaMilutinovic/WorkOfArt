81375b37c1454317d29b91f675168723ebbb57bf
pn200680d <pn200680d@student.etf.bg.ac.rs>
2023-07-02 04:49:17 +0200

    TODO functions done!
    Implemented filter, search, adding offers and sending message functions.

--- woa/src/views/art-concrete-view.vue
+++ woa/src/views/art-concrete-view.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="art-concrete-view-container">
     <app-header rootClassName="header-root-class-name1"></app-header>
-    <SingleArtComponent
-      :img_desc='this.art.data[this.lang].description' 
-      :image_src='this.art.image_src'
-      :name='this.art.data[this.lang].title'
-      :author='this.art.data[this.lang].author'>
+    <SingleArtComponent :img_desc='this.art.data[this.lang].description' :image_src='this.art.image_src'
+      :name='this.art.data[this.lang].title' :author='this.art.data[this.lang].author'>
     </SingleArtComponent>
 
     <div class="offers-and-messages">
@@ -13,42 +10,36 @@
         {{ viewAll[lang] }}
       </button>
 
-    <div class="art-concrete-view-container1">
-      <span class="art-concrete-view-text08">
-        <span>{{enterOffer[lang]}}</span>
-        <br />
-      </span>
-      <textarea
-        placeholder="offer"
-        class="art-concrete-view-textarea textarea"
-      ></textarea>
-      <button class="art-concrete-view-button button" @click="offer()">
-        {{ offerButton[lang] }}
-      </button>
+      <div class="art-concrete-view-container1">
+        <span class="art-concrete-view-text08">
+          <span>{{ enterOffer[lang] }}</span>
+          <br />
+        </span>
+        <textarea placeholder="offer" class="art-concrete-view-textarea textarea" name="offerText"
+          v-model="offerText"></textarea>
+        <button class="art-concrete-view-button button" @click="offer()">
+          {{ offerButton[lang] }}
+        </button>
+      </div>
+      <div class="art-concrete-view-container1">
+        <span class="art-concrete-view-text14">
+          <span>{{ enterMessage[lang] }}</span>
+          <br />
+
+        </span>
+        <textarea placeholder="message" class="art-concrete-view-textarea textarea" name="messageText"
+          v-model="messageText"></textarea>
+        <button class="art-concrete-view-button button" @click="send()">
+          {{ sendButton[lang] }}
+        </button>
+      </div>
     </div>
-    <div class="art-concrete-view-container1">
-      <span class="art-concrete-view-text14">
-        <span>{{ enterMessage[lang] }}</span>
-        <br />
-        
-      </span>
-      <textarea
-        placeholder="message"
-        class="art-concrete-view-textarea textarea"
-      ></textarea>
-      <button class="art-concrete-view-button button" @click="send()">
-        {{ sendButton[lang] }}
-      </button>
+
+
+    <div v-if="this.allOff == true">
+      <ArtAllOffersComponent :title='this.art.data[this.lang].title'></ArtAllOffersComponent>
     </div>
-    </div>
-    
-    
-    <div v-if="this.allOff==true">
-      <ArtAllOffersComponent 
-        :title='this.art.data[this.lang].title'  
-      ></ArtAllOffersComponent>
-    </div>
-    
+
     <AppGallery :id='this.id'></AppGallery>
     <app-footer rootClassName="footer-root-class-name1"></app-footer>
   </div>
@@ -77,8 +68,8 @@ export default {
       default: 1
     },
   },
-  data(){
-    return{
+  data() {
+    return {
       lang: 0,
       arts: [],
       art: {},
@@ -87,36 +78,101 @@ export default {
       offerButton: ['Ponudi', 'Offer'],
       sendButton: ['Posalji', 'Send'],
       enterOffer: ['Unesite svoju ponudu', 'Enter your offer'],
-      enterMessage: ['Unesite svoju poruku', 'Enter your message']
+      enterMessage: ['Unesite svoju poruku', 'Enter your message'],
+      messageText: '',
+      offerText: '',
     }
   },
-  created(){
-    if(localStorage.getItem('lang')==null){
+  created() {
+    if (localStorage.getItem('lang') == null) {
       localStorage.setItem('lang', 0);
     }
     this.lang = JSON.parse(localStorage.getItem('lang'))
 
-    if (localStorage.getItem('arts')==null)
+    if (localStorage.getItem('arts') == null)
       localStorage.setItem('arts', JSON.stringify(AllArts))
-    
-    this.arts = AllArts
-    
-    this.art = this.arts.find(art => art.id == this.id);
-    
-  },
-  methods:{
-    view(){
-      this.allOff = !this.allOff;
-      //TODO filtrirati ponude i poruke samo za datu umetninu
-    },
-    offer(){
-      //TODO poslati ponudu za dato delo (dodati u offers koji je u localTSorage)
-      //samo za ulogovane korisnike (localStorage.getItem("user") vraca nesto sto nije null)
-      //tip = 0
-    },
-    send(){
-      //TODO isto kao offer samo je tip 1
 
+    this.arts = AllArts
+
+    this.art = this.arts.find(art => art.id == this.id);
+
+  },
+  methods: {
+    view() {
+      this.allOff = !this.allOff;
+
+    },
+    offer() {
+      const user = localStorage.getItem("user");
+
+      // Check if the user is logged in
+      if (user !== null) {
+
+        // Create the offer object
+        const offer = {
+          id: this.id,
+          image_src: this.art.image_src,
+          type: 0,
+          data: [
+            {
+              title: this.art.data[this.lang].title,
+              author: this.art.data[this.lang].author,
+              user: user.name,
+              description: this.offerText
+            },
+
+          ]
+        };
+
+        // Get existing offers from localStorage
+        const offers = JSON.parse(localStorage.getItem("offers")) || [];
+
+        // Add the new offer to the array
+        offers.push(offer);
+
+        // Update the offers in localStorage
+        localStorage.setItem("offers", JSON.stringify(offers));
+      }
+      else {
+        this.$router.push('/login');
+
+      }
+    },
+    send() {
+      const user = localStorage.getItem("user");
+
+      // Check if the user is logged in
+      if (user !== null) {
+
+        // Create the offer object
+        const offer = {
+          id: this.id,
+          image_src: this.art.image_src,
+          type: 1,
+          data: [
+            {
+              title: this.art.data[this.lang].title,
+              author: this.art.data[this.lang].author,
+              user: user.name,
+              description: this.messageText
+            },
+
+          ]
+        };
+
+        // Get existing offers from localStorage
+        const offers = JSON.parse(localStorage.getItem("offers")) || [];
+
+        // Add the new offer to the array
+        offers.push(offer);
+
+        // Update the offers in localStorage
+        localStorage.setItem("offers", JSON.stringify(offers));
+      }
+      else {
+        this.$router.push('/login');
+
+      }
     }
   }
 }
@@ -130,6 +186,7 @@ export default {
   flex-direction: row;
   justify-content: space-between;
 }
+
 .art-concrete-view-text14 {
   margin-top: 32px;
   text-align: center;
@@ -145,29 +202,33 @@ export default {
   flex-direction: row;
   justify-content: space-between;
 }
-.art-concrete-view-text08{
+
+.art-concrete-view-text08 {
   margin-top: 32px;
   text-align: center;
   padding-left: 48px;
   margin-bottom: 32px;
   padding-right: 48px;
 }
+
 .art-concrete-view-button {
   transition: 0.3s;
   margin: 16px;
   padding: 12px;
   color: #ffffff;
-  
+
   align-self: center;
   font-style: normal;
   font-weight: 600;
   background-color: #303030;
   border-radius: 8px;
 }
+
 .art-concrete-view-button:hover {
   transform: scale(1.02);
 }
-.art-concrete-view-textarea{
+
+.art-concrete-view-textarea {
   width: 100%;
   margin: 24px;
   max-width: 100%;
@@ -175,7 +236,8 @@ export default {
   border: 1px solid #303030;
   border-radius: 8px;
 }
-.offers-and-messages{
+
+.offers-and-messages {
   width: 100%;
   display: flex;
   padding: 48px;
@@ -186,6 +248,7 @@ export default {
   flex-direction: column;
   justify-content: center;
 }
+
 .art-concrete-view-container {
   width: 100%;
   display: flex;
--- woa/src/views/art-overview-view.vue
+++ woa/src/views/art-overview-view.vue
@@ -4,40 +4,30 @@
     <div class="art-overview-view-container1">
       <div class="art-overview-view-container2">
         <select class="art-overview-view-select" name="choice" v-model="choice">
-          <option value="0" >{{choices[0][lang]}}</option>
-          <option value="1">{{choices[1][lang]}}</option>
-          <option value="2">{{choices[2][lang]}}</option>
+          <option value="0">{{ choices[0][lang] }}</option>
+          <option value="1">{{ choices[1][lang] }}</option>
+          <option value="2">{{ choices[2][lang] }}</option>
         </select>
-        <button type="button" class="art-overview-view-button button"
-                @click="sortBy()">
-          {{sort[lang]}}
+        <button type="button" class="art-overview-view-button button" @click="sortBy()">
+          {{ sort[lang] }}
         </button>
       </div>
       <div class="art-overview-view-container3">
-        <input
-          type="text"
-          placeholder="ex. Van Gogh"
-          class="art-overview-view-textinput input"
-          name="filter" v-model="filter"
-        />
-        <button type="button" class="art-overview-view-button button"
-                @click="searchBy()">
-          {{search[lang]}}
+        <input type="text" placeholder="ex. Van Gogh" class="art-overview-view-textinput input" name="filter"
+          v-model="filter" />
+        <button type="button" class="art-overview-view-button button" @click="searchBy()">
+          {{ search[lang] }}
         </button>
       </div>
     </div>
     <div class="art-overview-view-blog" v-for="art in this.artShowing" :key="art.id">
-      <div class="art-overview-view-container4" v-if="art.type==this.type">
+      <div class="art-overview-view-container4" v-if="art.type == this.type">
         <router-link :to="`/art-concrete-view/${art.id}`" class="art-overview-view-navlink">
-          <ArtOverviewCardComponent1
-            class="art-overview-view-component1"
-            :image_src='art.image_src'
-            :author1='art.data[this.lang].author'
-            :title='art.data[this.lang].title'
-          ></ArtOverviewCardComponent1>
+          <ArtOverviewCardComponent1 class="art-overview-view-component1" :image_src='art.image_src'
+            :author1='art.data[this.lang].author' :title='art.data[this.lang].title'></ArtOverviewCardComponent1>
         </router-link>
       </div>
-      
+
     </div>
     <app-footer></app-footer>
   </div>
@@ -48,7 +38,7 @@ import AppHeader from '../components/header'
 import ArtOverviewCardComponent1 from '../components/art-overview-card-component1'
 import AppFooter from '../components/footer'
 import AllArt from '../data/arts.js'
- 
+
 export default {
   name: 'ArtOverviewView',
   components: {
@@ -62,8 +52,8 @@ export default {
       default: 0
     },
   },
-  data(){
-    return{
+  data() {
+    return {
       artShowing: [],
       allArts: [],
       search: ['Pretrazi', 'Search'],
@@ -71,40 +61,62 @@ export default {
       lang: 0,
       choice: 0,
       filter: '',
-      choices: [['Izaberi', 'Choose'], 
-                ['Po umetniku', 'By artist'],
-                ['Po nazivu', 'By title']]
+      choices: [['Izaberi', 'Choose'],
+      ['Po umetniku', 'By artist'],
+      ['Po nazivu', 'By title']]
     }
   },
-  created(){
+  created() {
     this.allArts = AllArt
     this.artShowing = this.allArts.filter(art => art.type == this.type)
     this.lang = localStorage.getItem('lang')
   },
   methods: {
-    sortBy(){
-      //TODO
-      alert(this.choice);
-      switch (this.choice){
-        case 0: 
-          this.artShowing = AllArt;
-          break;
-        case 1:
-          //po imenu autora
-          break;
-        case 2:
-          //po nazivu
-          break;
-      }
-      
+    sortBy() {
+      switch (this.choice) {
+    case 0:
+      // No sorting, show all arts as is
+      this.artShowing = this.allArts;
+      break;
+    case 1:
+      // Sort by author name
+      this.artShowing = this.allArts.slice().sort((a, b) => {
+        const authorA = a.data[this.lang].author.toLowerCase();
+        const authorB = b.data[this.lang].author.toLowerCase();
+        return authorA.localeCompare(authorB);
+      });
+      break;
+    case 2:
+      // Sort by title
+      this.artShowing = this.allArts.slice().sort((a, b) => {
+        const titleA = a.data[this.lang].title.toLowerCase();
+        const titleB = b.data[this.lang].title.toLowerCase();
+        return titleA.localeCompare(titleB);
+      });
+      break;
+    default:
+      break;
+  }
+
 
     },
-    searchBy(){
-      //TODO filter je ili ime autora ili ime umetnine
-      //mozes direktno poredenje ne moras se muciti
-      alert(this.filter);
+    searchBy() {
+
+      const keyword = this.filter.toLowerCase().trim();
+
+      // Filter arts based on the keyword
+      const filteredArts = this.allArts.filter(art => {
+        const title = this.art.data[this.lang].title.toLowerCase();
+        const author = this.art.data[this.lang].author.toLowerCase();
+        const description = this.art.data[this.lang].description.toLowerCase();
+
+        return title.includes(keyword) || author.includes(keyword) || description.includes(keyword);
+      });
+
+      // Update the displayed arts
+      this.artShowing = filteredArts;
     }
-  
+
   }
 }
 </script>
@@ -119,6 +131,7 @@ export default {
   flex-direction: column;
   justify-content: center;
 }
+
 .art-overview-view-container1 {
   flex: 0 0 auto;
   width: 100%;
@@ -128,6 +141,7 @@ export default {
   padding-right: 32px;
   justify-content: space-between;
 }
+
 .art-overview-view-container2 {
   flex: 0 0 auto;
   width: 50%;
@@ -135,12 +149,14 @@ export default {
   align-items: flex-start;
   flex-direction: row;
 }
+
 .art-overview-view-select {
   margin: 16px;
   border: 1px solid #303030;
   padding: 6px;
   border-radius: 8px;
 }
+
 .art-overview-view-button {
   margin: 16px;
   padding-top: 4px;
@@ -154,6 +170,7 @@ export default {
   background-color: #303030;
   border-radius: 8px;
 }
+
 .art-overview-view-container3 {
   flex: 0 0 auto;
   width: 50%;
@@ -162,12 +179,14 @@ export default {
   flex-direction: row;
   justify-content: flex-end;
 }
+
 .art-overview-view-textinput {
   margin: 16px;
   border: 1px solid #303030;
   padding: 6px;
   border-radius: 8px;
 }
+
 .art-overview-view-blog {
   width: 100%;
   display: flex;
@@ -177,6 +196,7 @@ export default {
   flex-direction: column;
   justify-content: space-between;
 }
+
 .art-overview-view-container4 {
   width: 100%;
   display: flex;
@@ -185,12 +205,15 @@ export default {
   flex-direction: column;
   justify-content: space-between;
 }
+
 .art-overview-view-navlink {
   display: contents;
 }
+
 .art-overview-view-component1 {
   text-decoration: none;
 }
+
 .art-overview-view-container5 {
   width: 100%;
   display: flex;
@@ -199,12 +222,15 @@ export default {
   flex-direction: column;
   justify-content: space-between;
 }
+
 .art-overview-view-navlink1 {
   display: contents;
 }
+
 .art-overview-view-component2 {
   text-decoration: none;
 }
+
 @media(max-width: 991px) {
   .art-overview-view-container1 {
     width: 100%;
@@ -212,34 +238,38 @@ export default {
     flex-direction: column;
     justify-content: flex-start;
   }
+
   .art-overview-view-container2 {
     width: 100%;
     align-items: flex-start;
     justify-content: flex-start;
   }
+
   .art-overview-view-container3 {
     width: 100%;
     align-items: flex-start;
     justify-content: flex-start;
   }
 }
+
 @media(max-width: 767px) {
   .art-overview-view-blog {
     padding-left: 32px;
     padding-right: 32px;
   }
 }
+
 @media(max-width: 479px) {
   .art-overview-view-textinput {
     margin-left: 16px;
     padding-left: 16px;
     padding-right: 16px;
   }
+
   .art-overview-view-blog {
     padding-top: 32px;
     padding-left: 16px;
     padding-right: 16px;
     padding-bottom: 32px;
   }
-}
-</style>
+}</style>
